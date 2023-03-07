ef356c93ca19814bb8aeb8bc4199ea7d36a30e71
momotvit <110504519+momotvit@users.noreply.github.com>
2023-03-06 22:16:02 -0800

    fetch()

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const button = document.querySelector("button");
+// const button = document.querySelector("button");
 
-const onClick = () => {
-    setTimeout(() => {
-      console.log("I love async JS!");
-      alert("I love async JS!");
+// const onClick = () => {
+//     setTimeout(() => {
+//       console.log("I love async JS!");
+//       alert("I love async JS!");
       
       
-  }, 2000);
-};
+//   }, 2000);
+// };
 
 // Two seconds after clicking the button, 
 // alert specified inside the setTimeout callback will appear.
-button.addEventListener("click", onClick);
+// button.addEventListener("click", onClick);
 
 
 
 ///////////clearTimeOut (timeOut is not going to work)
 
 
-const greet = () => {
-  console.log("Hello!");
-};
+// const greet = () => {
+//   console.log("Hello!");
+// };
 
-const timerId1 = setTimeout(greet, 3000);
+// const timerId1 = setTimeout(greet, 3000);
 
-clearTimeout(timerId1);
+// clearTimeout(timerId1);
 
 
 
@@ -41,22 +41,22 @@ clearTimeout(timerId1);
 
 
 
-const startBtn = document.querySelector(".js-start");
-const stopBtn = document.querySelector(".js-stop");
-let timerId = null;
+// const startBtn = document.querySelector(".js-start");
+// const stopBtn = document.querySelector(".js-stop");
+// let timerId = null;
 
 
-startBtn.addEventListener("click", () => {
-  timerId = setInterval(() => {
-    console.log(`I love async JS!  ${Math.random()}`);
-  }, 1000);
-});
+// startBtn.addEventListener("click", () => {
+//   timerId = setInterval(() => {
+//     console.log(`I love async JS!  ${Math.random()}`);
+//   }, 1000);
+// });
 
 
-stopBtn.addEventListener("click", () => {
-  clearInterval(timerId);
-  console.log(`Interval with id ${timerId} has stopped!`);
-});
+// stopBtn.addEventListener("click", () => {
+//   clearInterval(timerId);
+//   console.log(`Interval with id ${timerId} has stopped!`);
+// });
 
 
 
@@ -64,77 +64,77 @@ stopBtn.addEventListener("click", () => {
 ///////// class Date
 
 
-const date = new Date();
+// const date = new Date();
 
-console.log(date);
-// "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
+// console.log(date);
+// // "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
 
-console.log(date.toString());
-// "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
-
-
-console.log(new Date(0));
-// "Thu Jan 01 1970 03:00:00 GMT+0300 (Eastern European Standard Time)"
-
-console.log(new Date(15000));
-// "Thu Jan 01 1970 03:00:15 GMT+0300 (Eastern European Standard Time)"
+// console.log(date.toString());
+// // "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
 
 
+// console.log(new Date(0));
+// // "Thu Jan 01 1970 03:00:00 GMT+0300 (Eastern European Standard Time)"
 
-///////// method getTime()
+// console.log(new Date(15000));
+// // "Thu Jan 01 1970 03:00:15 GMT+0300 (Eastern European Standard Time)"
 
-confirm(new Date().getTime());
-console.log(new Date().getTime());
+
+
+// ///////// method getTime()
+
+// confirm(new Date().getTime());
+// console.log(new Date().getTime());
 
 
 ////can set the time as a string
 
-const teamMeetingDate = new Date("March 16, 2030");
-console.log(teamMeetingDate);
-// "Mon Mar 16 2030 00:00:00 GMT+0200 (Eastern European Standard Time)"
+// const teamMeetingDate = new Date("March 16, 2030");
+// console.log(teamMeetingDate);
+// // "Mon Mar 16 2030 00:00:00 GMT+0200 (Eastern European Standard Time)"
 
-const preciseTeamMeetingDate = new Date("March 16, 2030 14:25:00");
-console.log(preciseTeamMeetingDate);
-// "Mon Mar 16 2030 14:25:00 GMT+0200 (Eastern European Standard Time)"
-
-
-////can set the time as a number
+// const preciseTeamMeetingDate = new Date("March 16, 2030 14:25:00");
+// console.log(preciseTeamMeetingDate);
+// // "Mon Mar 16 2030 14:25:00 GMT+0200 (Eastern European Standard Time)"
 
 
-const date1 = new Date(2030, 2, 16, 14, 25, 0, 0);
-console.log(date1);
-// Sat Mar 16 2030 14:25:00 GMT+0200 (Eastern European Standard Time)
+// ////can set the time as a number
+
+
+// const date1 = new Date(2030, 2, 16, 14, 25, 0, 0);
+// console.log(date1);
+// // Sat Mar 16 2030 14:25:00 GMT+0200 (Eastern European Standard Time)
 
 
 
 // ////////date getter
 
-// const date = new Date();
-console.log("Date: ", date);
+// // const date = new Date();
+// console.log("Date: ", date);
 
-// Возвращает день месяца от 1 до 31
-console.log("getDate(): ", date.getDate());
+// // Возвращает день месяца от 1 до 31
+// console.log("getDate(): ", date.getDate());
 
-// Возвращает день недели от 0 до 6
-console.log("getDay(): ", date.getDay());
+// // Возвращает день недели от 0 до 6
+// console.log("getDay(): ", date.getDay());
 
-// Возвращает месяц от 0 до 11 , works like index
-console.log("getMonth(): ", date.getMonth());
+// // Возвращает месяц от 0 до 11 , works like index
+// console.log("getMonth(): ", date.getMonth());
 
-// Возвращает год из 4 цифр
-console.log("getFullYear(): ", date.getFullYear());
+// // Возвращает год из 4 цифр
+// console.log("getFullYear(): ", date.getFullYear());
 
-// Возвращает час
-console.log("getHours(): ", date.getHours());
+// // Возвращает час
+// console.log("getHours(): ", date.getHours());
 
-// Возвращает минуты
-console.log("getMinutes(): ", date.getMinutes());
+// // Возвращает минуты
+// console.log("getMinutes(): ", date.getMinutes());
 
-// Возвращает секунды
-console.log("getSeconds(): ", date.getSeconds());
+// // Возвращает секунды
+// console.log("getSeconds(): ", date.getSeconds());
 
-// Возвращает миллисекунды
-console.log("getMilliseconds(): ", date.getMilliseconds());
+// // Возвращает миллисекунды
+// console.log("getMilliseconds(): ", date.getMilliseconds());
 
 
 
@@ -143,31 +143,31 @@ console.log("getMilliseconds(): ", date.getMilliseconds());
 //  адаптированные к текущему часовому поясу пользователя.
 
 
-console.log("Date: ", date);
+// console.log("Date: ", date);
 
-// Возвращает день месяца от 1 до 31
-console.log("getUTCDate(): ", date.getUTCDate());
+// // Возвращает день месяца от 1 до 31
+// console.log("getUTCDate(): ", date.getUTCDate());
 
-// Возвращает день недели от 0 до 6
-console.log("getUTCDay(): ", date.getUTCDay());
+// // Возвращает день недели от 0 до 6
+// console.log("getUTCDay(): ", date.getUTCDay());
 
-// Возвращает месяц от 0 до 11
-console.log("getUTCMonth(): ", date.getUTCMonth());
+// // Возвращает месяц от 0 до 11
+// console.log("getUTCMonth(): ", date.getUTCMonth());
 
-// Возвращает год из 4 цифр
-console.log("getUTCFullYear(): ", date.getUTCFullYear());
+// // Возвращает год из 4 цифр
+// console.log("getUTCFullYear(): ", date.getUTCFullYear());
 
-// Возвращает час
-console.log("getUTCHours(): ", date.getUTCHours());
+// // Возвращает час
+// console.log("getUTCHours(): ", date.getUTCHours());
 
-// Возвращает минуты
-console.log("getUTCMinutes(): ", date.getUTCMinutes());
+// // Возвращает минуты
+// console.log("getUTCMinutes(): ", date.getUTCMinutes());
 
-// Возвращает секунды
-console.log("getUTCSeconds(): ", date.getUTCSeconds());
+// // Возвращает секунды
+// console.log("getUTCSeconds(): ", date.getUTCSeconds());
 
-// Возвращает миллисекунды
-console.log("getUTCMilliseconds(): ", date.getUTCMilliseconds());
+// // Возвращает миллисекунды
+// console.log("getUTCMilliseconds(): ", date.getUTCMilliseconds());
 
 
 
@@ -186,26 +186,57 @@ console.log("getUTCMilliseconds(): ", date.getUTCMilliseconds());
 ////formatting date format
 
 
-date.toString();
-// "Sat Mar 16 2030 14:25:00 GMT+0200 (Eastern European Standard Time)"
+// date.toString();
+// // "Sat Mar 16 2030 14:25:00 GMT+0200 (Eastern European Standard Time)"
 
-date.toTimeString();
-// "14:25:00 GMT+0200 (Eastern European Standard Time)"
+// date.toTimeString();
+// // "14:25:00 GMT+0200 (Eastern European Standard Time)"
 
-date.toLocaleTimeString();
-// "2:25:00 PM"
+// date.toLocaleTimeString();
+// // "2:25:00 PM"
 
-date.toUTCString();
-// "Sat, 16 Mar 2030 12:25:00 GMT"
+// date.toUTCString();
+// // "Sat, 16 Mar 2030 12:25:00 GMT"
 
-date.toDateString();
-// "Sat Mar 16 2030"
+// date.toDateString();
+// // "Sat Mar 16 2030"
 
-date.toISOString();
-// "2030-03-16T12:25:00.000Z"
+// date.toISOString();
+// // "2030-03-16T12:25:00.000Z"
 
-date.toLocaleString();
-// "3/16/2030, 2:25:00 PM"
+// date.toLocaleString();
+// // "3/16/2030, 2:25:00 PM"
 
-date.getTime();
-// 1899894300000
+// date.getTime();
+// // 1899894300000
+
+
+
+//   fetch("https://jsonplaceholder.typicode.com/users")
+//   .then(response => response.json())
+//   .then(json => console.log(json))
+//   .catch(error => {error.json(console.error);
+//     // Error handling
+//   });
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then(response => response.text())
+//   .then(text => console.log(text))
+//   .catch(error => {error.json(console.error);
+//     // Error handling
+//   });
+
+  fetch("https://jsonplaceholder.typicode.com/users")
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(console.log(response.status));
+    }
+    console.log(response);
+    return response.json();
+  })
+  .then(data => {
+    // Data handling
+  })
+  .catch(error => {
+    // Error handling
+  });
