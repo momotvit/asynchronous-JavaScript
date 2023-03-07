7cd981e32a02c9fb0e228a47ac84220098c88ead
momotvit <110504519+momotvit@users.noreply.github.com>
2023-03-07 00:56:11 -0800

    URLSearchParams, globalVariable,getting 7 users in alphabetical order

--- index.js
+++ index.js
@@ -233,13 +233,102 @@
 //   .then(data => { })
 //   .catch(error => { });
 
-fetch("https://jsonplaceholder.typicode.com/users")
-  .then(response => {
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error(console.log(response.status));
+//     }
+//     console.log(response.blob());
+//     return response.blob();
+//   })
+//   .then(data => { })
+//   .catch(error => { });
+
+
+//   fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//   .then(json => console.log(json))
+      
+
+
+  ///////////////////////////////// when press fetch users btn fn renders users
+
+
+
+const fetchUsersBtn = document.querySelector(".btn");
+const userList = document.querySelector(".user-list");
+
+fetchUsersBtn.addEventListener("click", () => {
+  fetchUsers()
+    .then((users) => renderUserList(users))
+    .catch((error) => console.log(error));
+});
+
+function fetchUsers() {
+  ///////////////////////////////////getting 7 users in alphabetical order
+  return fetch( "https://jsonplaceholder.typicode.com/users?_limit=7&_sort=name"
+    
+  // return fetch("https://jsonplaceholder.typicode.com/users")
+  // getting all of the users not in alphabetical order
+  ).then((response) => {
     if (!response.ok) {
-      throw new Error(console.log(response.status));
+      throw new Error(response.status);
     }
-    console.log(response.blob());
-    return response.blob();
-  })
-  .then(data => { })
-  .catch(error => { });
+    return response.json();
+  });
+}
+
+function renderUserList(users) {
+  const markup = users
+    .map((user) => {
+      return `
+          <li>
+            <p><b>Name</b>: ${user.name}</p>
+            <p><b>Email</b>: ${user.email}</p>
+            <p><b>Company</b>: ${user.company.name}</p>
+          </li>
+      `;
+    })
+    .join("");
+  userList.innerHTML = markup;
+}
+
+
+/////////////////////////////////////// common mistake that new sevelopers make globalVariable outside fetch
+
+
+
+let globalVariable; // undefined
+
+// Initializing data fetching
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then(response => response.json())
+  .then(users => {
+    console.log("users inside then callback: ", users);
+
+    // Writing the result to a global variable
+    globalVariable = users;
+
+    // Everything is ok here, the data is in the variable
+    console.log("globalVariable inside fetch callback: ", globalVariable);
+  });
+
+// No async data here
+console.log("globalVariable outside fetch: ", globalVariable); // undefined
+
+
+
+
+/////////////////////////////////////////////  URLSearchParams()
+
+
+const searchParams = new URLSearchParams({
+  _limit: 5,
+  _sort: "name",
+});
+
+console.log(searchParams.toString()); // "_limit=5&_sort=name"
+console.log(searchParams);
+
+const url = `https://jsonplaceholder.typicode.com/users?${searchParams}`;
+console.log(url); // "https://jsonplaceholder.typicode.com/users?_limit=5&_sort=name"
