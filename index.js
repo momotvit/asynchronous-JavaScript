efd9faae9f0d5cb551222abc587ad8f47cc88ed2
momotvit <110504519+momotvit@users.noreply.github.com>
2023-03-12 19:53:17 -0700

    CRUD is awsome

--- index.js
+++ index.js
@@ -433,76 +433,161 @@
 
 //////////////////////////////////data limits //////////////////////////////////
 
-const fetchPostsBtn = document.querySelector(".btn");
-const userList = document.querySelector(".posts");
-const alertPopup = document.querySelector(".alert");
-let isAlertVisible = false;
+// const fetchPostsBtn = document.querySelector(".btn");
+// const userList = document.querySelector(".posts");
+// const alertPopup = document.querySelector(".alert");
+// let isAlertVisible = false;
 
-// Controls the group number
-let page = 1;
-// Controls the number of items in the group
-let limit = 50;
-// In our case total number of pages is calculated on frontend
-const totalPages = 100 / limit;
+// // Controls the group number
+// let page = 1;
+// // Controls the number of items in the group
+// let limit = 50;
+// // In our case total number of pages is calculated on frontend
+// const totalPages = 100 / limit;
 
-fetchPostsBtn.addEventListener("click", () => {
-  // Check the end of the collection to display an alert
-  if (page > totalPages) {
-    return toggleAlertPopup();
-  }
+// fetchPostsBtn.addEventListener("click", () => {
+//   // Check the end of the collection to display an alert
+//   if (page > totalPages) {
+//     return toggleAlertPopup();
+//   }
 
-  fetchPosts()
-    .then((posts) => {
-      renderPosts(posts);
-      // Increase the group number
-      page += 1;
+//   fetchPosts()
+//     .then((posts) => {
+//       renderPosts(posts);
+//       // Increase the group number
+//       page += 1;
 
-      // Replace button text after first request
-      if (page > 1) {
-        fetchPostsBtn.textContent = "Fetch more posts";
-      }
-    })
-    .catch((error) => console.log(error));
-});
+//       // Replace button text after first request
+//       if (page > 1) {
+//         fetchPostsBtn.textContent = "Fetch more posts";
+//       }
+//     })
+//     .catch((error) => console.log(error));
+// });
 
-function fetchPosts() {
-  const params = new URLSearchParams({
-    _limit: limit,
-    _page: page
-  });
+// function fetchPosts() {
+//   const params = new URLSearchParams({
+//     _limit: limit,
+//     _page: page
+//   });
 
-  return fetch(`https://jsonplaceholder.typicode.com/posts?${params}`).then(
-    (response) => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    }
-  );
-}
+//   return fetch(`https://jsonplaceholder.typicode.com/posts?${params}`).then(
+//     (response) => {
+//       if (!response.ok) {
+//         throw new Error(response.status);
+//       }
+//       return response.json();
+//     }
+//   );
+// }
 
-function renderPosts(posts) {
-  const markup = posts
-    .map(({ id, title, body, userId }) => {
-      return `<li>
-          <h2 class="post-title">${title.slice(0, 30)}</h2>
-          <p><b>Post id</b>: ${id}</p>
-          <p><b>Author id</b>: ${userId}</p>
-          <p class="post-body">${body}</p>
-        </li>`;
-    })
-    .join("");
-  userList.insertAdjacentHTML("beforeend", markup);
-}
+// function renderPosts(posts) {
+//   const markup = posts
+//     .map(({ id, title, body, userId }) => {
+//       return `<li>
+//           <h2 class="post-title">${title.slice(0, 30)}</h2>
+//           <p><b>Post id</b>: ${id}</p>
+//           <p><b>Author id</b>: ${userId}</p>
+//           <p class="post-body">${body}</p>
+//         </li>`;
+//     })
+//     .join("");
+//   userList.insertAdjacentHTML("beforeend", markup);
+// }
 
-function toggleAlertPopup() {
-  if (isAlertVisible) {
-    return;
-  }
-  isAlertVisible = true;
-  alertPopup.classList.add("is-visible");
-  setTimeout(() => {
-    alertPopup.classList.remove("is-visible");
-    isAlertVisible = false;
-  }, 3000);
-}
+// function toggleAlertPopup() {
+//   if (isAlertVisible) {
+//     return;
+//   }
+//   isAlertVisible = true;
+//   alertPopup.classList.add("is-visible");
+//   setTimeout(() => {
+//     alertPopup.classList.remove("is-visible");
+//     isAlertVisible = false;
+//   }, 3000);
+// }
+
+
+
+
+
+
+////////////////////////////////CRUD
+//////////////adding post data to storage, method POST
+
+
+
+//   const postToAdd = {
+//   author: "Mango",
+//   body: "CRUD is awesome",
+// };
+
+// const options = {
+//   method: "POST",
+//   body: JSON.stringify(postToAdd),
+//   headers: {
+//     "Content-Type": "application/json; charset=UTF-8",
+//   },
+// };
+
+// fetch("https://jsonplaceholder.typicode.com/posts", options)
+//   .then(response => response.json())
+//   .then(post => console.log(post))
+//   .catch(error => console.log(error));
+
+
+
+
+  ///////// checking if added
+
+// const postId = 10;///////chenge the value
+
+// fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+//   .then(response => response.json())
+//   .then(post => console.log(post))
+//   .catch(error => console.log(error));
+
+
+  /////////////////PUT and PATCH
+
+  
+  // Change value of id property to update different post
+const postToUpdate = {
+  id: 1,
+  body: "CRUD is really awesome",
+};
+
+const options = {
+  method: "PATCH",
+  body: JSON.stringify(postToUpdate),
+  headers: {
+    "Content-Type": "application/json; charset=UTF-8",
+  },
+};
+
+fetch(`https://jsonplaceholder.typicode.com/posts/${postToUpdate.id}`, options)
+  .then(response => response.json())
+  .then(post => console.log(post))
+  .catch(error => console.log("ERROR" + error));
+
+  const postId = 4;///////chenge the value
+
+fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+  .then(response => response.json())
+  .then(post => console.log(post))
+  .catch(error => console.log(error));
+
+
+
+
+  //////////////// method DELETE
+
+
+
+  const postIdToDelete = 4;
+
+fetch(`https://jsonplaceholder.typicode.com/posts/${postIdToDelete}`, {
+  method: "DELETE",
+})
+  .then(() => console.log("Post deleted"))
+  .catch(error => console.log("Error:", error));
